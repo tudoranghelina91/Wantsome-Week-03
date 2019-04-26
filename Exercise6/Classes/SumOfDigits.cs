14f7fbf53c117302cb7df0b5896b665ac5630eed
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-04-26 14:08:17 +0300

    Completed Exercise 6

---
+++ Exercise6/Classes/SumOfDigits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6.Classes
+{
+    class SumOfDigits
+    {
+        public static int Sum(int n)
+        {
+            int s = 0;
+
+            if (n == 0)
+            {
+                return s;
+            }
+
+            else
+            {
+                return s + n % 10 + Sum (n / 10);
+            }
+        }
+    }
+}
---
+++ Exercise6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise6.Classes;
+using Wantsome_Week_03.Handlers;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void RunProgram(ref bool programRunning)
+        {
+            int x = InputHandling.ReadValue("Number to calculate the sum: ");
+            OutputHandling.Message("The sum of the digits of " + x + " is: " + SumOfDigits.Sum(x));
+            OutputHandling.Question("Do you want to calculate the sum of the digits of another number? Y / N");
+            programRunning = InputHandling.QuestionOptions();
+        }
+
+        static void Main(string[] args)
+        {
+            bool programRunning = true;
+            while (programRunning)
+            {
+                RunProgram(ref programRunning);
+            }
+        }
+    }
+}
