ec25a191eb49075bc5c234921da575b6b0af62d8
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-04-27 18:04:05 +0300

    Added Exercise 11

---
+++ Exercise11/Classes/Fibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11.Classes
+{
+    class Fibonacci
+    {
+        public static int FindFiboInSequence(int n)
+        {
+            int x = 1;
+            int y = 1;
+
+            if (n > 2)
+            {
+                int finalNumber = 0;
+                int i = 3;
+                while (i <= n)
+                {
+                    if (i <= n)
+                    {
+                        x += y;
+                        finalNumber = x;
+                        i++;
+                    }
+
+                    if (i <= n)
+                    {
+                        y += x;
+                        finalNumber = y;
+                        i++;
+                    }
+                }
+
+                return finalNumber;
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ Exercise11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wantsome_Week_03.Handlers;
+using Exercise11.Classes;
+namespace Exercise11
+{
+    class Program
+    {
+        static void RunProgram(ref bool programRunning)
+        {
+            int n = InputHandling.ReadValue("Number to print from fibonacci sequence, starting at 1: ");
+            Console.WriteLine(Fibonacci.FindFiboInSequence(n));
+            OutputHandling.Question("Do you want to find another number in a fibonacci sequence? Y / N");
+            programRunning = InputHandling.QuestionOptions();
+        }
+
+        static void Main(string[] args)
+        {
+            bool programRunning = true;
+            while (programRunning)
+            {
+                RunProgram(ref programRunning);
+            }
+        }
+    }
+}
