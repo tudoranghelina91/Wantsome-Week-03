c93c336e4b1eaa37c64d59dcb870fdc974d9165f
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-04-29 23:26:37 +0300

    Resolved Exercise20 and Exercise21

---
+++ Exercise20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wantsome_Week_03.Handlers;
+using Wantsome_Week_03.ArrayOperations;
+
+namespace Exercise20
+{
+    class Program
+    {
+        static void RunProgram(ref bool programRunning)
+        {
+            OutputHandling.Message("This program accepts an array and sorts it using Selection Sort");
+
+            int arrLen = InputHandling.ReadArrayLength();
+            int[] arr = new int[arrLen];
+            int lastFailIndex = 0;
+
+            InputHandling.ReadArrayElements(ref arr, arrLen, ref lastFailIndex);
+            Sorting.SelectSort(ref arr, arrLen);
+            OutputHandling.PrintArray(arr, arrLen, "Sorted array using Selection sort: ");
+            OutputHandling.Question("Do you want to sort another array with selection sort? Y / N");
+
+            programRunning = InputHandling.QuestionOptions();
+        }
+
+        static void Main(string[] args)
+        {
+            bool programRunning = true;
+            while (programRunning)
+            {
+                RunProgram(ref programRunning);
+            }
+        }
+    }
+}
---
+++ Exercise21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wantsome_Week_03.Handlers;
+using Wantsome_Week_03.ArrayOperations;
+
+namespace Exercise21
+{
+    class Program
+    {
+        static void RunProgram(ref bool programRunning)
+        {
+            OutputHandling.Message("This program accepts an array and sorts it using Bubble Sort");
+
+            int arrLen = InputHandling.ReadArrayLength();
+            int[] arr = new int[arrLen];
+            int lastFailIndex = 0;
+
+            InputHandling.ReadArrayElements(ref arr, arrLen, ref lastFailIndex);
+            Sorting.BubbleSort(ref arr, arrLen);
+            OutputHandling.PrintArray(arr, arrLen, "Sorted array using Bubble sort: ");
+            OutputHandling.Question("Do you want to sort another array with Bubble sort? Y / N");
+
+            programRunning = InputHandling.QuestionOptions();
+        }
+
+        static void Main(string[] args)
+        {
+            bool programRunning = true;
+            while (programRunning)
+            {
+                RunProgram(ref programRunning);
+            }
+        }
+    }
+}
