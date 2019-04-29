9e666d347b62e4162f52db7f1dedfc4214bfe323
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-04-29 15:53:23 +0300

    Resolved Exercise16

---
+++ Exercise16/Classes/Prime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16.Classes
+{
+    class Prime
+    {
+        public static bool IsItPrime(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return false;
+            }
+
+            else if (n % 2 == 0 && n != 2)
+            {
+                return false;
+            }
+
+            else
+            {
+                for (int i = 3; i <= Math.Sqrt(n); i += 2)
+                {
+                    if (n % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Exercise16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wantsome_Week_03.Handlers;
+using Exercise16.Classes;
+
+namespace Exercise16
+{
+    class Program
+    {
+        static void RunProgram(ref bool programRunning)
+        {
+            int n = InputHandling.ReadValue("Number to check if it is a prime number or not: ");
+            bool isPrime = Prime.IsItPrime(n);
+
+            if (isPrime)
+            {
+                OutputHandling.Message(n + " is a prime number!", ConsoleColor.Green);
+            }
+
+            else
+            {
+                OutputHandling.Message(n + " is not a prime number!", ConsoleColor.Red);
+            }
+
+            OutputHandling.Question("Do you want to check if another number is a prime number or not? Y / N");
+            programRunning = InputHandling.QuestionOptions();
+        }
+
+        static void Main(string[] args)
+        {
+            bool programRunning = true;
+            while (programRunning)
+            {
+                RunProgram(ref programRunning);
+            }
+        }
+    }
+}
