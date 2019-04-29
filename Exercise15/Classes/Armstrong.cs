2ffa8d0c67aeec8748b9e97937fb07df4e5a1407
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-04-29 15:36:06 +0300

    Resolved Exercise15

---
+++ Exercise15/Classes/Armstrong.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise15.Classes
+{
+    class Armstrong
+    {
+        public static bool IsArmstrong(int n)
+        {
+            int armstrong = 0;
+            if (n < 10)
+            {
+                return true;
+            }
+
+            else if (n >= 10 && n < 100)
+            {
+                return false;
+            }
+
+            else
+            {
+                int m = n;
+                int digitCount = 0;
+                while (m != 0)
+                {
+                    m /= 10;
+                    digitCount++;
+                }
+
+                m = n;
+
+                while (m != 0)
+                {
+                    int x = 1;
+                    for (int i = 1; i <= digitCount; i++)
+                    {
+                        x *= m % 10;
+                    }
+
+                    armstrong += x;
+                    m /= 10;
+                }
+
+                if (armstrong == n)
+                {
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+    }
+}
---
+++ Exercise15/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wantsome_Week_03.Handlers;
+using Exercise15.Classes;
+
+namespace Exercise15
+{
+    class Program
+    {
+        static void RunProgram(ref bool programRunning)
+        {
+            int n = InputHandling.ReadValue("Number to check if it is ARMSTRONG or NOT: ");
+            bool armstrong = Armstrong.IsArmstrong(n);
+
+            if (armstrong)
+            {
+                OutputHandling.Message(n + " is an armstrong number",ConsoleColor.Green);
+            }
+
+            else
+            {
+                OutputHandling.Message(n + " is not an armstrong number", ConsoleColor.Magenta);
+            }
+
+            OutputHandling.Question("Do you want to check another number if it is an Armstrong number? Y / N");
+            programRunning = InputHandling.QuestionOptions();
+        }
+
+        static void Main(string[] args)
+        {
+            bool programRunning = true;
+            while (programRunning)
+            {
+                RunProgram(ref programRunning);
+            }
+        }
+    }
+}
