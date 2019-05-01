adb6b5c75145a21a118061de8a91d40d3938d9f6
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-05-01 23:31:52 +0300

    Resolved Exercise14

---
+++ Exercise14/Classes/ThirdFromEnd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wantsome_Week_03;
+
+namespace Exercise14.Classes
+{
+    public class ThirdFromEnd
+    {
+        public static int FindThirdFromEnd(SingleLinkedList singleLinkedList)
+        {
+            int thirdFromEnd = 0;
+            SingleLinkedListNode singleLinkedListNode = singleLinkedList.head;
+
+            while(singleLinkedListNode.next.next != null)
+            {
+                thirdFromEnd = singleLinkedListNode.nodeData;
+                singleLinkedListNode = singleLinkedListNode.next;
+            }
+
+            return thirdFromEnd;
+        }
+    }
+}
---
+++ Exercise14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wantsome_Week_03;
+using Wantsome_Week_03.Handlers;
+using Wantsome_Week_03.SingleLinkedListOperations;
+using Exercise14.Classes;
+
+namespace Exercise14
+{
+    class Program
+    {
+        static void RunProgram(ref bool programRunning)
+        {
+            int myListLength = InputHandling.ReadCollectionLength("Singly Linked List Length: ");
+            SingleLinkedList myList = new SingleLinkedList();
+            int failIndex = 0;
+            InputHandling.ReadCollectionElements(ref myList, myListLength, ref failIndex);
+            int thirdFromEnd = ThirdFromEnd.FindThirdFromEnd(myList);
+
+            OutputHandling.Message("The third element counting from the end is: " + thirdFromEnd, ConsoleColor.Green);
+            OutputHandling.Question("Do you want to find another third last element of a linked list? Y / N");
+            programRunning = InputHandling.QuestionOptions();
+        }
+
+        static void Main(string[] args)
+        {
+            bool programRunning = true;
+            while (programRunning)
+            {
+                RunProgram(ref programRunning);
+            }
+        }
+    }
+}
